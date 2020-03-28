56ef14967f9c4b9b92e877c2a3134bcc041aeb49
Raidro <mateus_sat@hotmail.com>
2020-03-27 22:20:40 -0300

    Adiciona os botoes de listar tudo e por periodo

--- screens/tela_principal/index.js
+++ screens/tela_principal/index.js
@@ -1,7 +1,16 @@
 import React, { Component } from 'react';
 
 //importes dos componentes do  Native Base
-import { Container, Text, Picker, Content, Subtitle, H1, H4 } from "native-base";
+import {
+    Container,
+    Text,
+    Picker,
+    Content,
+    H1,
+    Separator,
+    ListItem,
+    Button
+} from "native-base";
 import { StyleSheet } from 'react-native';
 
 //importes de style
@@ -12,7 +21,7 @@ export default class tela_principal extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            selected: "key0"
+            selected: "SELECIONE UMA MOEDA"
         };
     }
     onValueChange(value) {
@@ -24,8 +33,6 @@ export default class tela_principal extends Component {
     render() {
         return (
             <Container style={styles.container}>
-
-
 
                 <H1 style={styles.title}>
                     World Coins
@@ -42,26 +49,60 @@ export default class tela_principal extends Component {
                         selectedValue={this.state.selected}
                         onValueChange={this.onValueChange.bind(this)}
                     >
-                        <Picker.Item label="SELECIONE UMA MOEDA" value="key0" />
-                        <Picker.Item label="USD - DOLAR COMERCIAL" value="key1" />
-                        <Picker.Item label="USDT - DOLAR TURISMO" value="key2" />
-                        <Picker.Item label="CAD - DOLAR CANADENSE" value="key3" />
-                        <Picker.Item label="AUD - DOLAR AUTRALIANO" value="key4" />
-                        <Picker.Item label="GBP - LIBRA ESTERLINA" value="key5" />
-                        <Picker.Item label="ARS - PESO ARGENTINO" value="key6" />
-                        <Picker.Item label="JPY - IENE JAPONES" value="key7" />
-                        <Picker.Item label="CHF - FRENCO SUIÇO" value="key8" />
-                        <Picker.Item label="CNY - YUAN CHINES" value="key9" />
-                        <Picker.Item label="YLS - NOVO SHEKEL ISRAELENSE" value="key10" />
-                        <Picker.Item label="BCT - BITCOIN" value="key11" />
-                        <Picker.Item label="LCT - LITECOIN" value="key12" />
-                        <Picker.Item label="ETH - ETHEREUM" value="key13" />
-                        <Picker.Item label="XRP - RIPPLE" value="key14" />
-
-
-
+                        <Picker.Item label="SELECIONE UMA MOEDA" value="SELECIONE UMA MOEDA" />
+                        <Picker.Item label="USD - DOLAR COMERCIAL" value="USD - DOLAR COMERCIAL" />
+                        <Picker.Item label="USDT - DOLAR TURISMO" value="USDT - DOLAR TURISMO" />
+                        <Picker.Item label="CAD - DOLAR CANADENSE" value="CAD - DOLAR CANADENSE" />
+                        <Picker.Item label="AUD - DOLAR AUTRALIANO" value="AUD - DOLAR AUTRALIANO" />
+                        <Picker.Item label="GBP - LIBRA ESTERLINA" value="GBP - LIBRA ESTERLINA" />
+                        <Picker.Item label="ARS - PESO ARGENTINO" value="ARS - PESO ARGENTINO" />
+                        <Picker.Item label="JPY - IENE JAPONES" value="JPY - IENE JAPONES" />
+                        <Picker.Item label="CHF - FRENCO SUIÇO" value="CHF - FRENCO SUIÇO" />
+                        <Picker.Item label="CNY - YUAN CHINES" value="CNY - YUAN CHINES" />
+                        <Picker.Item label="YLS - NOVO SHEKEL ISRAELENSE" value="YLS - NOVO SHEKEL ISRAELENSE" />
+                        <Picker.Item label="BCT - BITCOIN" value="BCT - BITCOIN" />
+                        <Picker.Item label="LCT - LITECOIN" value="LCT - LITECOIN" />
+                        <Picker.Item label="ETH - ETHEREUM" value="ETH - ETHEREUM" />
+                        <Picker.Item label="XRP - RIPPLE" value="XRP - RIPPLE" />
                     </Picker>
+
+                    <Separator bordered>
+                        <Text>Moeda: {this.state.selected}</Text>
+                    </Separator>
+                    <ListItem>
+                        <Text>Compra: </Text>
+                    </ListItem>
+
+                    <ListItem>
+                        <Text>Venda: </Text>
+                    </ListItem>
+
+                    <ListItem>
+                        <Text>Variação: </Text>
+                    </ListItem>
+
+                    <ListItem>
+                        <Text>Variação(%): </Text>
+                    </ListItem>
+
+                    <ListItem>
+                        <Text>Maximo: </Text>
+                    </ListItem>
+
+                    <ListItem last>
+                        <Text>Minimo</Text>
+                    </ListItem>
+
+                    <Button info>
+                        <Text style={styles.text}> Por Periodo </Text>
+                    </Button>
+
+                    <Button info style={styles.espaceButton}>
+                        <Text style={styles.text2}> Listar Todas </Text>
+                    </Button>
+
                 </Content>
+
 
             </Container>
         );
@@ -84,13 +125,24 @@ const styles = StyleSheet.create({
     },
 
     subtittle: {
-        marginTop: 30,
+        marginTop: 15,
         color: 'black',
 
     },
     pickers: {
         width: 220,
-        marginTop: 50
+        marginTop: 10
+    },
+    text: {
+        marginLeft: 45
+
+    },
+    text2: {
+        marginLeft: 45
+
+    },
+    espaceButton: {
+        marginTop: 15
     }
 });
 
